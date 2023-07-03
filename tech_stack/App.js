524340728fa1cc3155a59017ec7c99d7af2bf0d8
Dawit-Dev <100426510+Dawit-Dev@users.noreply.github.com>
2023-07-04 00:12:55 +0100

    Implemented the connect function

--- tech_stack/App.js
+++ tech_stack/App.js
@@ -3,12 +3,14 @@ import { Provider } from "react-redux";
 import { createStore } from "redux";
 import reducers from "./src/reducers";
 import { Header } from './src/components/common'
+import LibraryList from "./src/components/LibraryList";
 
 export default function App() {
   return (
     <Provider store={createStore(reducers)}>
       <View>
         <Header headerText="Tech Stack" />
+        <LibraryList />
       </View>
     </Provider>
   );
