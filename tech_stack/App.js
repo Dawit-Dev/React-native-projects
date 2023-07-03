53cbfa11e01054bb2e3b9daa3ff4b4f6f92a44cf
Dawit-Dev <100426510+Dawit-Dev@users.noreply.github.com>
2023-07-03 20:22:40 +0100

    Rendering the Header

--- tech_stack/App.js
+++ tech_stack/App.js
@@ -1,25 +1,24 @@
-import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
 import reducers from "./src/reducers";
+import { Header } from './src/components/common'
 
 export default function App() {
   return (
     <Provider store={createStore(reducers)}>
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <StatusBar style="auto" />
+      <View>
+        <Header headerText="Tech Stack" />
       </View>
     </Provider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+// });
