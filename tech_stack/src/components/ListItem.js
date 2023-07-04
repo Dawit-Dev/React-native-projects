754d947da36e5f500b6ca40f776c718142b084f1
Dawit-Dev <100426510+Dawit-Dev@users.noreply.github.com>
2023-07-04 18:27:12 +0100

    Adding a Touchable

--- tech_stack/src/components/ListItem.js
+++ tech_stack/src/components/ListItem.js
@@ -1,18 +1,26 @@
 import React, { Component } from "react";
-import { Text } from "react-native";
+import { Text, TouchableWithoutFeedback, View } from "react-native";
 import { connect } from "react-redux";
 import { CardSection } from "./common";
-import * as actions from '../actions'
+import * as actions from "../actions";
 
 class ListItem extends Component {
   render() {
-    const { titleStyle } = styles;
+      const { titleStyle } = styles;
+      const { id, title } = this.props.library;
+      
     return (
-      <CardSection>
+        <TouchableWithoutFeedback
+        onPress={() => this.props.selectLibrary(id)}
+        >
+        <View>
+          <CardSection>
             <Text style={titleStyle}>
-                {this.props.library.title}
+                {title}
             </Text>
-      </CardSection>
+          </CardSection>
+        </View>
+      </TouchableWithoutFeedback>
     );
   }
 }
