73c1018af3d9a283c9b14139a67f62da784fc96b
Dawit-Dev <100426510+Dawit-Dev@users.noreply.github.com>
2023-07-12 13:27:21 +0100

    Displaying a multiple screen

--- manager/src/Router.js
+++ manager/src/Router.js
@@ -3,14 +3,24 @@ import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import LoginForm from "./components/LoginForm";
+import EmployeeList from "./components/EmployeeList";
 
 const Stack = createNativeStackNavigator();
 
 const Router = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Please Login" component={LoginForm} title="Login" />
+      <Stack.Navigator initialRouteName="LoginForm">
+        <Stack.Screen
+          name="Please Login"
+          component={LoginForm}
+          options={{ title: "Login" }}
+        />
+        <Stack.Screen
+          name="Employees"
+          component={EmployeeList}
+          options={{ title: "Employee List" }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ manager/src/components/EmployeeList.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const EmployeeList = () => {
+  return (
+    <View>
+      <Text>Employee List</Text>
+      <Text>Employee List</Text>
+      <Text>Employee List</Text>
+      <Text>Employee List</Text>
+      <Text>Employee List</Text>
+      <Text>Employee List</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({})
+
+export default EmployeeList
