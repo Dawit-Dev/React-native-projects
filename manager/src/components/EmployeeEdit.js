1124d79422c2f9611d5e3d6f525e95b48514e359
Dawit-Dev <100426510+Dawit-Dev@users.noreply.github.com>
2023-07-18 19:54:53 +0100

    Implement reusable Modal component

--- manager/src/components/EmployeeEdit.js
+++ manager/src/components/EmployeeEdit.js
@@ -1,6 +1,7 @@
 import _ from "lodash";
 import React, { useEffect } from "react";
 import { connect } from "react-redux";
+import Communications from "react-native-communications";
 import EmployeeForm from "./EmployeeForm";
 import { employeeUpdate, employeeSave } from "../actions";
 import { StyleSheet } from "react-native";
@@ -14,15 +15,23 @@ const EmployeeEdit = (props) => {
   }, []);
 
   const onButtonPress = () => {
-    const { name, phone, shift } = props
-    props.employeeSave({ name, phone, shift, uid: props.employee.uid })
-}
+    const { name, phone, shift } = props;
+    props.employeeSave({ name, phone, shift, uid: props.employee.uid });
+  };
+
+  const onTextPress = () => {
+    const { phone, shift } = props;
+    Communications.text(phone, `Your upcoming shift is on ${shift}`);
+  };
 
   return (
     <Card>
       <EmployeeForm />
       <CardSection>
         <Button onPress={onButtonPress}>Save Changes</Button>
+      </CardSection>
+      <CardSection>
+        <Button onPress={onTextPress}>Text Schedule</Button>
       </CardSection>
     </Card>
   );
@@ -31,9 +40,11 @@ const EmployeeEdit = (props) => {
 const styles = StyleSheet.create({});
 
 const mapStateToProps = (state) => {
-  const { name, phone, shift } = state.EmployeeForm
+  const { name, phone, shift } = state.EmployeeForm;
 
-  return { name, phone, shift }
-}
+  return { name, phone, shift };
+};
 
-export default connect(mapStateToProps, { employeeUpdate, employeeSave })(EmployeeEdit);
+export default connect(mapStateToProps, { employeeUpdate, employeeSave })(
+  EmployeeEdit
+);
---
+++ manager/src/components/common/Confirm.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { StyleSheet, Text, View, Modal } from 'react-native'
+import { CardSection } from './CardSection'
+import { Button } from './Button'
+
+const Confirm = () => {
+  return (
+    <View>
+      <Text>Confirm</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
+
+export { Confirm }
